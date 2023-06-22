94fd15fae775c35848089e01c6f1bc5a4d83ca0f
jay mbans <jordan@mbanefo.com>
2023-06-21 23:32:16 -0400

    added work routes for minion + fixed issue where minion updates were not persistent

--- server/minions.js
+++ server/minions.js
@@ -1,5 +1,5 @@
 const minionsRouter = require('express').Router();
-const { getAllFromDatabase, addToDatabase, getFromDatabaseById, deleteFromDatabasebyId } = require('./db');
+const { getAllFromDatabase, addToDatabase, getFromDatabaseById, deleteFromDatabasebyId, updateInstanceInDatabase } = require('./db');
 
 
 minionsRouter.param('minionId', (req, res, next, id) => {
@@ -11,6 +11,18 @@ minionsRouter.param('minionId', (req, res, next, id) => {
 
   req.minion = minion;
   req.minionId = id;
+  next();
+})
+
+minionsRouter.param('workId', (req, res, next, id) => {
+  const work = getFromDatabaseById('work', id);
+
+  if (!work) {
+    return res.status(400).send('work not found')
+  }
+
+  req.work = work;
+  req.workId = id;
   next();
 })
 
@@ -33,12 +45,32 @@ minionsRouter.put('/:minionId', (req, res, next) => {
   const newMinion = req.body;
   req.minion = newMinion;
 
+  updateInstanceInDatabase('minions', newMinion)
+
   return res.status(201).send(newMinion)
 })
 
 minionsRouter.delete('/:minionId', (req, res, next) => {
 
   return res.status(201).send(deleteFromDatabasebyId('minions', req.minionId))
+})
+
+// work bonus section
+minionsRouter.get('/:minionId/work', (req, res) => {
+  return res.send(getAllFromDatabase('work'))
+})
+
+minionsRouter.post('/:minionId/work', (req, res) => {
+  return res.send(addToDatabase('work', req.body))
+})
+
+minionsRouter.put('/:minionId/work/:workId', (req, res) => {
+  const newWork = req.body;
+  req.work = newWork;
+
+  updateInstanceInDatabase('work', newWork)
+
+  return res.send(req.work)
 })
 
 
