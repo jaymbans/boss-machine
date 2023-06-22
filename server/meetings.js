9ab07b2eb6037343e0d537f2c1d607d1576f276c
jay mbans <jordan@mbanefo.com>
2023-06-21 23:09:21 -0400

    meetings route completed

--- server/meetings.js
+++ server/meetings.js
@@ -1,8 +1,20 @@
 const meetingsRouter = require('express').Router();
-const { getAllFromDatabase } = require('./db');
+const { getAllFromDatabase, deleteAllFromDatabase } = require('./db');
 
 meetingsRouter.get('/', (req, res, next) => {
   return res.status(200).send(getAllFromDatabase('meetings'));
+})
+
+
+meetingsRouter.post('/', (req, res, next) => {
+
+  const newMeeting = addToDatabase('meetings', req.body)
+  res.status(201).send(newMeeting)
+})
+
+meetingsRouter.delete('/', (req, res, next) => {
+
+  return res.send(deleteAllFromDatabase('meetings'));
 })
 
 module.exports = meetingsRouter;
