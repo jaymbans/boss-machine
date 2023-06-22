8ac60116bc5af1be9dbe4433637531cad6b4d3a4
jay mbans <jordan@mbanefo.com>
2023-06-21 23:09:06 -0400

    ideas route completed

--- server/ideas.js
+++ server/ideas.js
@@ -1,8 +1,40 @@
 const ideasRouter = require('express').Router();
-const { getAllFromDatabase } = require('./db');
+const { getAllFromDatabase, addToDatabase, getFromDatabaseById, deleteFromDatabasebyId } = require('./db');
+
+ideasRouter.param('ideaId', (req, res, next, id) => {
+  const idea = getFromDatabaseById('ideas', id);
+
+  if (!idea) {
+    return res.status(400).send('idea not found')
+  }
+
+  req.idea = idea;
+  req.ideaId = id;
+  next();
+})
 
 ideasRouter.get('/', (req, res, next) => {
   return res.status(200).send(getAllFromDatabase('ideas'));
+})
+
+ideasRouter.post('/', (req, res, next) => {
+  const newIdea = addToDatabase('ideas', req.body)
+  res.status(201).send(newIdea)
+})
+
+ideasRouter.get('/:ideaId', (req, res, next) => {
+  return res.send(req.idea);
+})
+
+ideasRouter.put('/:ideaId', (req, res, next) => {
+  const newIdea = req.body;
+  req.idea = newIdea;
+
+  return res.status(201).send(newIdea)
+})
+
+ideasRouter.delete('/:ideaId', (req, res, next) => {
+  return res.status(201).send(deleteFromDatabasebyId('ideas', req.ideaId))
 })
 
 module.exports = ideasRouter;
